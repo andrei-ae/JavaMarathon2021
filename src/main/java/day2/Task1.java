66150e4ad90dc27b525335b5a75d9f5dbadf95a0
andrei_ae <onlys@mail.ru>
2021-02-23 16:00:58 +0300

    Day 2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int nFloors = scanner.nextInt();
+
+        if (nFloors < 1) {
+            System.out.println("Ошибка ввода");
+        } else if (nFloors < 5) {
+            System.out.println("Малоэтажный дом");
+        } else if (nFloors < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else {
+            System.out.println("Многоэтажный дом");
+        }
     }
 }
--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+//        System.out.println("Пожалуйста, введите два числа (через пробел):");
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if (num1 >= num2) {
+            System.out.println("Некорректный ввод");
+        } else {
+            for (int i = num1 + 1; i < num2; i++) {
+                if (i % 5 == 0 && i % 10 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
 }
