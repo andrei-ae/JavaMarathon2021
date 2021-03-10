d813b7f20004053cb74b28f9db0095e447a52d43
andrei_ae <onlys@mail.ru>
2021-03-10 18:40:16 +0300

    Day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,33 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class Task1 {
     public static void main(String[] args) {
+        File inputFile = new File("src/main/resources/shoes.csv");
+        File outputFile = new File("src/main/resources/missing_shoes.txt");
 
+        try {
+            Scanner scanner = new Scanner(inputFile);
+            String line;
+
+            PrintWriter writer = new PrintWriter(outputFile);
+
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                String[] lineData = line.split(";");
+
+                if (lineData[2].equals("0")) {
+                    line = String.join(", ", lineData);
+                    writer.println(line);
+                }
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
